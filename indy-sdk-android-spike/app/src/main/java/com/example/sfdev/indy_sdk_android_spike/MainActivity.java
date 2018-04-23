a3fa2ad311fc24dd2ee19732dccef972abb9a098
stachen <stanleychen24w@gmail.com>
2018-04-23 16:15:15 -0700

    Save button and view

--- indy-sdk-android-spike/app/src/main/java/com/example/sfdev/indy_sdk_android_spike/MainActivity.java
+++ indy-sdk-android-spike/app/src/main/java/com/example/sfdev/indy_sdk_android_spike/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.sfdev.indy_sdk_android_spike;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button);
+        final TextView textView = findViewById(R.id.text);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                textView.setText("Blah blah");
+            }
+        });
     }
 }
