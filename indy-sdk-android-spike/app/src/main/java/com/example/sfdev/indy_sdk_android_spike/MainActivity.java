ec7051581a1c53ac2757e9a8cbcf2a99e548da1d
Stanley Chen <stanley.chen@trustscience.com>
2018-04-23 13:41:09 -0700

    Project created

---
+++ indy-sdk-android-spike/app/src/main/java/com/example/sfdev/indy_sdk_android_spike/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sfdev.indy_sdk_android_spike;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
